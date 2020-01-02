3b9f077b0b81b1174719cc52bd0c6a51de03516d
ThankGod George <gthankgod@gmail.com>
2020-01-02 15:00:32 -0800

    New commit

--- client/src/components/layouts/Landing.js
+++ client/src/components/layouts/Landing.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Landing = () => {
     return (
-        <div className="landing">
-            <h2>Landing</h2>
+        <div className="container" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#333', color: '#fff', height: '90vh' }}>
+            <h1>FOGM Admin Member Tracker</h1>
         </div>
     )
 }
